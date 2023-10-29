35c2e242df772ca48b0540d908383c8ed3d71ee7
arufonsekun <junior.ramisch@gmail.com>
2023-10-29 12:22:46 -0300

    chore: Adds USER_STATUS and TOPICS constants

---
+++ src/constants/topics.js
@@ -0,0 +1,6 @@
+export default {
+    /**
+     * Tells which users are online and offline
+     */
+    USERS_ONLINE_TOPIC: "USERS_ONLINE",
+};
---
+++ src/constants/user-status.js
@@ -0,0 +1,4 @@
+export default {
+    OFFLINE: 0,
+    ONLINE: 1,
+}
