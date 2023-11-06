4c13d9c9f960af09b7efac241414f9e61ed2897c
arufonsekun <junior.ramisch@gmail.com>
2023-11-06 19:26:39 -0300

    chore: Adds group topic

--- src/constants/topics.js
+++ src/constants/topics.js
@@ -3,4 +3,9 @@ export default {
      * Tells which users are online and offline
      */
     USERS_ONLINE_TOPIC: "USERS_ONLINE",
+
+    /**
+     * Tells if a new group is created.
+     */
+    GROUPS_TOPIC: "GROUPS",
 };
