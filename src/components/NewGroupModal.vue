342cf8038e74b8f38278b95b81e89ce05dd69715
Guerezi <gabrielguerezi@hotmail.com>
2023-11-06 20:39:41 -0300

    chore: improve code style

--- src/components/NewGroupModal.vue
+++ src/components/NewGroupModal.vue
@@ -74,32 +74,16 @@ defineExpose({ show })
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title text-body no-text-selection">Novo grupo</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body text-body">
           <div class="input-group mb-3">
-            <input
-              type="text"
-              class="group-name form-control no-text-selection"
-              placeholder="Nome do grupo"
-              aria-label="Nome"
-              aria-describedby="button-addon2"
-              v-model="name"
-            />
+            <input type="text" class="group-name form-control no-text-selection" placeholder="Nome do grupo"
+              aria-label="Nome" aria-describedby="button-addon2" v-model="name" />
           </div>
           <div class="form-floating no-text-selection">
-            <textarea
-              class="form-control"
-              placeholder="Descrição do grupo"
-              id="description"
-              style="height: 100px"
-              v-model="description"
-            ></textarea>
+            <textarea class="form-control" placeholder="Descrição do grupo" id="description" style="height: 100px"
+              v-model="description"></textarea>
             <label for="description">Descrição</label>
           </div>
           <div class="row">
@@ -108,17 +92,8 @@ defineExpose({ show })
             </div>
             <div class="col-6" v-for="user in zapUsers" :key="user.uuid">
               <div class="form-check px-5">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  :value="user.uuid"
-                  :id="user.uuid"
-                />
-                <label
-                  class="form-check-label w-100 text-truncate no-text-selection"
-                  :title="user.name"
-                  :for="user.uuid"
-                >
+                <input class="form-check-input" type="checkbox" :value="user.uuid" :id="user.uuid" />
+                <label class="form-check-label w-100 text-truncate no-text-selection" :title="user.name" :for="user.uuid">
                   {{ user.name }}
                 </label>
               </div>
